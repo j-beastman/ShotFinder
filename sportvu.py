081f5405d184485580c78d4fd20f49fc7cf269b9
John Eastman <john@bee-haven.com>
2024-03-11 13:46:40 -0400

    started arcs

--- sportvu.py
+++ sportvu.py
@@ -44,28 +44,28 @@ def seconds_since_start(quarter, seconds_left):
     return ((quarter - 1) * 720) + (720 - seconds_left)
 
 ####################################################################################
-##  Convert raw data into something more usable 
+##  Convert raw data into something more usable, only use once
 ####################################################################################
-# # Open up the SportsVu data
-# with open('0021500495.json', 'r') as file:
-#     sportVU = json.load(file)
+def clean_data():
+    # Open up the SportsVu data
+    with open('0021500495.json', 'r') as file:
+        sportVU = json.load(file)
 
-# # Organize data!
-# all_moments = []
+    all_moments = []
 
-# # Iterate through each event
-# for event in sportVU['events']:
-#     # Iterate through each moment in the event and extend the all_moments list
-#     for moment in event['moments']:
-#         if moment not in all_moments:
-#             all_moments.append(moment)
+    # Iterate through each event
+    for event in sportVU['events']:
+        # Iterate through each moment in the event and extend the all_moments list
+        for moment in event['moments']:
+            if moment not in all_moments:
+                all_moments.append(moment)
 
-# # Specify the file path where you want to save the JSON file
-# file_path = "output.json"
+    # Specify the file path where you want to save the JSON file
+    file_path = "output.json"
 
-# # Write the list to a JSON file
-# with open(file_path, 'w') as json_file:
-#     json.dump(all_moments, json_file)
+    # Write the list to a JSON file
+    with open(file_path, 'w') as json_file:
+        json.dump(all_moments, json_file)
 
 
 # Now, we just read from our moments json file (the cleaned up one)
@@ -76,7 +76,10 @@ with open("output.json", 'r') as json_file:
 shot_times_list = []
 skip_to_index = 0
 index_of_list = -1
-shot_found = False
+
+####################################################################################
+# Iterate through each moment, if 
+####################################################################################
 for index, moment in enumerate(all_moments):
     if index < skip_to_index:
         continue
@@ -85,15 +88,18 @@ for index, moment in enumerate(all_moments):
     ball_location = Location(ball_info[2], ball_info[3], ball_info[4])
     # If the ball is above the rim, it has the potential to be a shot.
     if ball_location.Z > 10:
-        # If it is a shot,
         if is_shot(ball_location):
             seconds = seconds_since_start(quarter=moment[0], seconds_left=moment[2])
             timestamp = moment[1]
-            if not shot_found:
+            # If shot list is empty, we have to add to list
+            if len(shot_times_list) == 0:
                 shot_times_list.append((seconds, timestamp))
-                shot_found = True
+            # If the timestamp of the current shot is within 0.5 second (500 milliseconds)
+            #   of any other shots recorded in our shot_times array,
+            #   then we don't want to add to our shot_times array, this solves a bug that is 
+            #   present in the last ~10 shots--comment out and see what happens.
             elif any(timestamp == shot_time[1] for shot_time in shot_times_list) or (
-                                    abs(shot_times_list[index_of_list][1] - timestamp) < 1000):
+                                    abs(shot_times_list[index_of_list][1] - timestamp) < 500):
                 continue
             else:
                 shot_times_list.append((seconds, timestamp))
@@ -122,11 +128,11 @@ TRUE_SHOT_TOTAL = 231 # according to sportsreference
 print("Failed to find", (TRUE_SHOT_TOTAL - len(shot_times)), "shots!")
 
 # Code to print out shots (game time, realtime, from our array
-# testing = shot_times[:-5]
-# for i, item in enumerate(shot_times):
-#     for value in item:
-#         print("{:.1f}".format(value), end=' ')
-#     print("Shot number", i)
+testing = shot_times[:-5]
+for i, item in enumerate(shot_times):
+    for value in item:
+        print("{:.1f}".format(value), end=' ')
+    print("Shot number", i)
 
 shot_facts = np.arange(0, len(shot_times), 1)
 arc_lengths = shot_facts
@@ -135,52 +141,53 @@ arc_lengths = shot_facts
 # ###################################################################################
 # #  Part II: Calculate arc length of shot vs. distance to basketball        
 # ###################################################################################
-# def ball_in_air(moment, ball_location, threshold=2):
-#     # Loc_info is info of ball + players
-#     loc_info = moment[5]
-#     # Loop through all players, if any are within 'threshold' feet of ball, 
-#     for i in range(1, 10):
-#         player_info = loc_info[i]
-#         player_location = Location(player_info[2], player_info[3], player_info[4])
-#         if euclid_distance(player_location, ball_location) < threshold:
-#             return False
-#     return True
-# shot_arc_information = []
-# shots_found = 0
-# # shots_not_found = shot_times.copy()
-# for event in all_data["events"]:
-#     for i, moment in enumerate(event["moments"]):
-#         ball_info = moment[5][0]
-#         ball_location = Location(ball_info[2], ball_info[3], ball_info[4])
-#         if is_shot(ball_location):
-#             try: 
-#                 if abs(seconds_since_start(quarter=moment[0], seconds_left=moment[2]) - shot_times[shots_found]) < 1:
-#                     # print("Found a new shot", shots_found, "shots found")
-#                     shots_found += 1
-#                     in_air = True
-#                     ball_arc_information = []
-#                     # Go back in time until ball is in the hands of a player
-#                     try: 
-#                         # Loop while ball is in the air otherwise, we assume ball 
-#                         #  is in the hands of the shooter, so we stop taking in measurements
-#                         while in_air:
-#                             # The ball location is really what we care about here.
-#                             ball_arc_information.append(ball_location)
-#                             # Go back in time by 1 moment, if ball is still in air, then
-#                             #   we are at beginning of loop and we take another measurement
-#                             #   of its location.
-#                             past_moment = event["moments"][i]
-#                             ball_info = past_moment[5][0]
-#                             ball_location = Location(ball_info[2], ball_info[3], ball_info[4])
-#                             in_air:bool = ball_in_air(past_moment, ball_location)
-#                             i -= 1
-#                         shot_arc_information.append(ball_arc_information)
-#                     except IndexError:
-#                         shot_arc_information.append(ball_arc_information)
-#                         continue
-#             except IndexError:
-#                 print("Found all shots")
-#                 break
+def ball_in_air(moment, ball_location, threshold=2):
+    # Loc_info is info of ball + players
+    loc_info = moment[5]
+    # Loop through all players, if any are within 'threshold' feet of ball, 
+    for i in range(1, 10):
+        player_info = loc_info[i]
+        player_location = Location(player_info[2], player_info[3], player_info[4])
+        if euclid_distance(player_location, ball_location) < threshold:
+            return False
+    return True
+
+shot_arc_information = []
+shots_found = 0
+for index, moment in enumerate(all_moments):
+    ball_info = moment[5][0]
+    ball_location = Location(ball_info[2], ball_info[3], ball_info[4])
+    if is_shot(ball_location):
+        try: 
+            if abs(seconds_since_start(quarter=moment[0], seconds_left=moment[2]
+                                       ) - shot_times[shots_found][0]) < 1:
+                print("Found a new shot", shots_found, "shots found")
+                shots_found += 1
+                in_air = True
+                ball_arc_information = []
+                # Go back in time until ball is in the hands of a player
+                try: 
+                    # Loop while ball is in the air otherwise, we assume ball 
+                    #  is in the hands of the shooter, so we stop taking in measurements
+                    i = 0
+                    while in_air:
+                        # The ball location is really what we care about here.
+                        ball_arc_information.append(ball_location)
+                        # Go back in time by 1 moment, if ball is still in air, then
+                        #   we are at beginning of loop and we take another measurement
+                        #   of its location.
+                        past_moment = all_moments[index - i]
+                        ball_info = past_moment[5][0]
+                        ball_location = Location(ball_info[2], ball_info[3], ball_info[4])
+                        in_air:bool = ball_in_air(past_moment, ball_location)
+                        i += 1
+                    shot_arc_information.append(ball_arc_information)
+                except IndexError:
+                    shot_arc_information.append(ball_arc_information)
+                    continue
+        except IndexError:
+            print("Found all shots")
+            break
 # ###################################################################################
 # #  This little section is purely for sanity check
 # # Calculate the arc length of the shot
@@ -193,22 +200,23 @@ arc_lengths = shot_facts
 # ###################################################################################
 
 
-# def calculate_arc_length(shot_path):
-#     """
-#     Calculate the arc length for a given shot path.
+def calculate_arc_length(shot_path):
+    """
+    Calculate the arc length for a given shot path.
     
-#     Args:
-#     - shot_path: A list Location objects.
+    Args:
+    - shot_path: A list Location objects.
 
-#     Returns:
-#     - The total arc length of the path.
-#     """
-#     return sum(shot_path[i].distance_to(shot_path[i+1]) for i in range(len(shot_path)-1))
+    Returns:
+    - The total arc length of the path.
+    """
+    return sum(shot_path[i].distance_to(shot_path[i+1]) for i in range(len(shot_path)-1))
 
-# arc_lengths = np.array([calculate_arc_length(shot) for shot in shot_arc_information])
-# print(len(arc_lengths))
-# print(len(shot_times))
+arc_lengths = np.array([calculate_arc_length(shot) for shot in shot_arc_information])
+print(len(arc_lengths))
+print(len(shot_times))
 
+shot_times = list(map(lambda x: x[0], shot_times))
 # Now that we have our shot times, let's figure out our accuracy and sensitivity
 #   Confusion matrix (according to event dataset)
 # freethrowy poo
@@ -217,31 +225,31 @@ arc_lengths = shot_facts
 # This code creates the timeline display from the shot_times
 # and shot_facts arrays.
 # DO NOT MODIFY THIS CODE APART FROM THE SHOT FACT LABEL
-# fig, ax = plt.subplots(figsize=(12,3))
-# fig.canvas.manager.set_window_title('Shot Timeline')
+fig, ax = plt.subplots(figsize=(12,3))
+fig.canvas.manager.set_window_title('Shot Timeline')
 
-# plt.scatter(shot_times, np.full_like(shot_times, 0), marker='o', s=50, color='royalblue', edgecolors='black', zorder=3, label='shot')
-# plt.bar(shot_times, arc_lengths, bottom=2, color='royalblue', edgecolor='black', width=5, label='shot fact') # <- This is the label you can modify
+plt.scatter(shot_times, np.full_like(shot_times, 0), marker='o', s=50, color='royalblue', edgecolors='black', zorder=3, label='shot')
+plt.bar(shot_times, arc_lengths, bottom=2, color='royalblue', edgecolor='black', width=5, label='shot fact') # <- This is the label you can modify
 
-# ax.spines['bottom'].set_position('zero')
-# ax.spines['top'].set_color('none')
-# ax.spines['right'].set_color('none')
-# ax.spines['left'].set_color('none')
-# ax.tick_params(axis='x', length=20)
-# ax.xaxis.set_major_locator(matplotlib.ticker.FixedLocator([0,720,1440,2160,2880])) 
-# ax.set_yticks([])
+ax.spines['bottom'].set_position('zero')
+ax.spines['top'].set_color('none')
+ax.spines['right'].set_color('none')
+ax.spines['left'].set_color('none')
+ax.tick_params(axis='x', length=20)
+ax.xaxis.set_major_locator(matplotlib.ticker.FixedLocator([0,720,1440,2160,2880])) 
+ax.set_yticks([])
 
-# _, xmax = ax.get_xlim()
-# ymin, ymax = ax.get_ylim()
-# ax.set_xlim(-15, xmax)
-# ax.set_ylim(ymin, ymax+5)
-# ax.text(xmax, 2, "time", ha='right', va='top', size=10)
-# plt.legend(ncol=5, loc='upper left')
+_, xmax = ax.get_xlim()
+ymin, ymax = ax.get_ylim()
+ax.set_xlim(-15, xmax)
+ax.set_ylim(ymin, ymax+5)
+ax.text(xmax, 2, "time", ha='right', va='top', size=10)
+plt.legend(ncol=5, loc='upper left')
 
-# plt.tight_layout()
-# plt.show()
+plt.tight_layout()
+plt.show()
 
-# plt.savefig("Shot_Timeline.png")
+plt.savefig("Shot_Timeline.png")
 
 ###################################################################################
 # Below is omitted code which we used to figure out our false positives/negatives
